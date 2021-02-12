e7d21d060f80ff22f0dabc549f79c2a3bd43e8ba
Gazeddy <gazeddy@gmail.com>
2021-02-12 23:44:15 +0000

    arduino desoldering gun initial

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+const int Temp = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+
+void setup() {
+  //start serial connection
+  Serial.begin(9600);
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(Temp);
+  //read the pushbutton value into a variable
+  int switchVal = digitalRead(2);
+  //print out the value of the pushbutton
+ Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t switch = ");
+  Serial.println(switchVal);
+                
+  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (switchVal == HIGH && sensorValue <= 110 ) { 
+    digitalWrite(4, LOW);
+  } else {
+    digitalWrite(4, HIGH);
+  }
+  if (sensorValue >=135) {
+    digitalWrite(3, LOW);
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(3, HIGH);
+    digitalWrite(13, HIGH);
+  }
+}
