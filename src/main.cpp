98d4eabe2a0ac4f4f49869fe09b102c17ae305db
Gazeddy <gazeddy@gmail.com>
2021-02-15 00:37:41 +0000

    tidied

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <Arduino.h>
-const int Temp = A0;  // Analog input pin that the potentiometer is attached to
-int sensorValue = 0;        // value read from the pot
-
+const int Temp = A0;   
+int sensorValue = 0;    
 void setup() {
-  //start serial connection
   Serial.begin(9600);
-  //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(2, INPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(13, OUTPUT);
@@ -14,12 +10,9 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value:
   sensorValue = analogRead(Temp);
-  //read the pushbutton value into a variable
   int switchVal = digitalRead(2);
-  //print out the value of the pushbutton
- Serial.print("sensor = ");
+  Serial.print("sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t switch = ");
   Serial.println(switchVal);
