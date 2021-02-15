5f7383f6adb7bd72a2832ff214ad37b89bc40c48
Gazeddy <gazeddy@gmail.com>
2021-02-15 12:46:13 +0000

    added board.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <SPI.h>
-U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 8, /* data=*/ 10, /* CS=*/ 9, /* reset=*/ 13);
-const int Temp = A0;  // Analog input pin that the potentiometer is attached to
+#include <board.h>
+
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, lcdclock, lcddata,lcdchipselect, lcdreset);
 int sensorValue = 0;        // value read from the pot
 
 void setup() {
   //start serial connection
   Serial.begin(9600);
-  //configure pin 2 as an input and enable the internal pull-up resistor
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(switchPin, INPUT);
+  pinMode(Heatergate, OUTPUT);
+  pinMode(pumpgatePin, OUTPUT);
   pinMode(13, OUTPUT);
 
 }
@@ -31,18 +31,18 @@ void loop() {
   // HIGH when it's open, and LOW when it's pressed. Turn on inbuilt led when the
   // temp sensor is above setpoint and off when it's not:
   if (switchVal == HIGH) { 
-    digitalWrite(3, LOW);
+    digitalWrite(pumpgatePin, LOW);
     Serial.print("Pump Off ");
   } else {
-    digitalWrite(3, HIGH);
+    digitalWrite(pumpgatePin, HIGH);
     Serial.print("Pump On ");
   }
   if (sensorValue >=135) {
-    digitalWrite(2, LOW);
+    digitalWrite(Heatergate, LOW);
     digitalWrite(13, LOW);
     Serial.print("Heater Off ");
   } else {
-    digitalWrite(2, HIGH);
+    digitalWrite(Heatergate, HIGH);
     digitalWrite(13, HIGH);
     Serial.print("Heater On ");
     }
