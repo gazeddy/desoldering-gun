37c0fcd445d6b05994a4c9077a040de3d5442074
Gazeddy <gazeddy@gmail.com>
2021-02-15 12:02:30 +0000

    update comments

--- src/main.cpp
+++ src/main.cpp
@@ -17,29 +17,33 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value:
+  // read the temp value:
   sensorValue = analogRead(Temp);
-  //read the pushbutton value into a variable
-  int switchVal = digitalRead(2);
-  //print out the value of the pushbutton
+  //read the switch value into a variable
+  int switchVal = digitalRead(1);
+  //print out the value of the switch and temp sensor
  Serial.print("sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t switch = ");
   Serial.println(switchVal);
                 
-  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
-  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
-  // button's pressed, and off when it's not:
-  if (switchVal == HIGH ) { 
-    digitalWrite(4, LOW);
-  } else {
-    digitalWrite(4, HIGH);
-  }
-  if (sensorValue >=135) {
+  // Keep in mind the pull-up means the switches logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on inbuilt led when the
+  // temp sensor is above setpoint and off when it's not:
+  if (switchVal == HIGH) { 
     digitalWrite(3, LOW);
-    digitalWrite(13, LOW);
+    Serial.print("Pump Off ");
   } else {
     digitalWrite(3, HIGH);
-    digitalWrite(13, HIGH);
+    Serial.print("Pump On ");
   }
+  if (sensorValue >=135) {
+    digitalWrite(2, LOW);
+    digitalWrite(13, LOW);
+    Serial.print("Heater Off ");
+  } else {
+    digitalWrite(2, HIGH);
+    digitalWrite(13, HIGH);
+    Serial.print("Heater On ");
+    }
 }
